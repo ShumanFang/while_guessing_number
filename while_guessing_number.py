8e433e5d5adef00bd682f48cfd1c38b48eadc998
Shuman Fang <shuman.fang@gmail.com>
2018-07-24 21:06:02 -0400

    add code

--- while_guessing_number.py
+++ while_guessing_number.py
@@ -1,13 +1,17 @@
 import random 
 r = random.randint(1, 100)
-
+i = 0
 while True:
-	num = input('please guess the number: ')
+	i = i + 1 #same as: i += 1
+	print('this is your ', i, 'try')
+	num = input('please guess number: ')
 	num = int(num)
 	if num == r:
 		print('you got it right!')
+		print('this is your', i, 'try')
 		break
 	elif num > r:
 		print('your number is bigger than actual')
 	else:
 		print('your number is smaller than actual')
+ 
